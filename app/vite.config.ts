bb491b091402295b6b42b5c6f7ca40c5320896df
ebikatsu <ennir0508+pg@gmail.com>
2024-04-02 21:00:40 +0900

    add hot reload

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+  },
   plugins: [react()],
-})
+});
